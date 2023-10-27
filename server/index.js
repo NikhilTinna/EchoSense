bb8f6e40218e91289da32209cb25363b3fd9b742
abdulqader <abdulqaderpatel2002@gmail.com>
2023-10-27 11:55:18 +0530

    created models for user,posts and comment table

--- server/index.js
+++ server/index.js
@@ -1,17 +1,57 @@
-const express=require("express")
-const cors=require("cors")
-const bodyparser=require("body-parser")
-const cloudinary=require("cloudinary")
-const {PrismaClient}=require("@prisma/client")
-
-const app=express()
-
-app.get(express.json())
-app.get(cors())
-app.use(bodyparser.json())
+const express = require("express");
+const cors = require("cors");
+const bodyparser = require("body-parser");
+const cloudinary = require("cloudinary");
+const { PrismaClient } = require("@prisma/client");
 
 
-app.listen(3000,(()=>{
-    
-}))
+const app = express();
 
+const prisma = new PrismaClient();
+app.get(express.json());
+app.get(cors());
+app.use(bodyparser.json());
+
+app.get("/", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
+app.post("/signup",async(req,res)=>{
+    const newUser=await prisma.user.create({data:req.body})
+    res.json(newUser)
+})
+
+app.get("/posts/:id",async(req,res)=>{
+    const id=req.params.id;
+
+    const posts=await prisma.post.findMany({where:{userId:id},include:{
+        user:true,
+        quotePost:true
+    }})
+    res.json(posts)
+})
+
+app.post("/posts",async(req,res)=>{
+    const newPost=await prisma.post.create({data:req.body})
+    res.json(newPost)
+})
+
+app.post("/posts/reply",async(req,res)=>{
+    const newPost=await prisma.post.create({data:req.body})
+    res.json(newPost)
+})
+
+app.get("/comments/:id",async(req,res)=>{
+    const id=req.params.id;
+
+    const posts=
+    res.json(posts)
+})
+
+app.post("/comments",async(req,res)=>{
+    const newComment=await prisma.comment.create({data:req.body})
+    res.json(newComment)
+})
+
+app.listen(3000, () => {});
